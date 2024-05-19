d1623bda921dd05b09fafcf36d03a42dbeba46fa
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-05-19 19:44:10 -0400

    refactor: starting over

--- src/ui/components/app/App.tsx
+++ src/ui/components/app/App.tsx
@@ -1,5 +1,19 @@
-import { Bookshelf } from '../bookshelf/Bookshelf';
+import { createEffect, createSignal } from 'solid-js';
+
+const [count, setCount] = createSignal(0);
+
+type Assign<A, B> = A & Omit<B, keyof A>;
+type Foo = Assign<{ foo: string; bar: string }, { bar: number }>;
+
+const foo: Foo = {};
 
 export const App = () => {
-  return <Bookshelf name="Tramaine" />;
+  createEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log('count is ', count());
+  });
+
+  // setInterval(() => setCount(count() + 1), 1000);
+
+  return <></>;
 };
