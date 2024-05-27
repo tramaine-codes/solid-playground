01f2781cad908d92d9ab999a18bb6e36537e75f3
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-05-27 11:38:12 -0400

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solid()],
-  build: {
-    target: 'esnext',
-  },
-  server: {
-    port: 3000,
-  },
-  test: {
-    testTransformMode: {
-      web: ['*.[jt]sx'],
-    },
-    deps: { registerNodeLoader: false },
-  },
 });
